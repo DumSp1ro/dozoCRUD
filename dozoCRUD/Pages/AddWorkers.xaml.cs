c0da69aabb3df3c3eaaa87409dd870929e8c75db
dozopravka <dozopravka@example.com>
2023-11-11 01:28:12 +0300

    dozo

---
+++ dozoCRUD/Pages/AddWorkers.xaml.cs
@@ -0,0 +1,94 @@
+﻿using dozoCRUD.Classes;
+using dozoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dozoCRUD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWorkers.xaml
+    /// </summary>
+    public partial class AddWorkers : Page
+    {
+        private workers currentworker = new workers();
+        public AddWorkers(workers sellectedWorkers)
+        {
+            InitializeComponent();
+            if (sellectedWorkers != null)
+            {
+                currentworker = sellectedWorkers;
+            }
+            DataContext = currentworker;
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            if (currentworker.id == 0)
+            {
+                dozo.GetContext().workers.Add(currentworker);
+            }
+
+            DbContextTransaction dbContextTransaction = null;
+
+            try
+            {
+                if (currentworker.id == 0)
+                {
+                    dozo.GetContext().workers.Add(currentworker);
+                }
+
+                dbContextTransaction = dozo.GetContext().Database.BeginTransaction();
+
+                dozo.GetContext().SaveChanges();
+
+                MessageBox.Show("Информация сохранена!");
+                dbContextTransaction.Commit();
+                Manager.MainFrame.GoBack();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (dbContextTransaction != null)
+                {
+                    dbContextTransaction.Rollback();
+                }
+
+                var innerException = ex.InnerException;
+                while (innerException != null)
+                {
+                    MessageBox.Show($"Внутреннее исключение: {innerException.Message}");
+                    innerException = innerException.InnerException;
+                }
+
+                MessageBox.Show("Ошибка при сохранении изменений. Дополнительные сведения в внутреннем исключении.");
+            }
+            catch (Exception ex)
+            {
+                if (dbContextTransaction != null)
+                {
+                    dbContextTransaction.Rollback();
+                }
+
+                MessageBox.Show($"Ошибка при обновлении записей. Дополнительные сведения: {ex.Message}");
+            }
+            finally
+            {
+                dbContextTransaction?.Dispose();
+            }
+        }
+    }
+}
+
---
+++ dozoCRUD/Pages/work.xaml.cs
@@ -0,0 +1,62 @@
+﻿using dozoCRUD.Classes;
+using dozoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dozoCRUD.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для work.xaml
+    /// </summary>
+    public partial class work : Page
+    {
+        public work()
+        {
+            InitializeComponent();
+            BDWorkers.ItemsSource = dozo.GetContext().workers.ToList();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddWorkers((sender as Button).DataContext as workers));
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddWorkers(null));
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            var workersDell = BDWorkers.SelectedItems.Cast<workers>().ToList();
+
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {workersDell.Count()} элементов?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    dozo.GetContext().workers.RemoveRange(workersDell);
+                    dozo.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+
+                    BDWorkers.ItemsSource = dozo.GetContext().workers.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+    }
+}
